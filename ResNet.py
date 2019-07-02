bde8c0320a69ec5f9386986d67dcb0ea13998081
492235584 <492235584@qq.com>
2019-07-03 00:08:34 +0800

    reconstruct BasicBlock

--- ResNet.py
+++ ResNet.py
@@ -1,5 +1,48 @@
 import torch.nn as nn
 
+class BasicBlock(nn.Module):
+    expansions = 1  # how many time increase between first convolution and last convolution
+
+    def __init__(self, in_c, out_c, downsample):
+        '''
+        init BasicBlock
+
+        :param in_c: number of input channel
+        :param out_c: number of output channel
+        :param downsample: convolution of downsample
+        '''
+        super(BasicBlock, self).__init__()
+        self.downsample = downsample
+
+        if downsample:
+            self.conv1 = Conv2d_BN(in_c, out_c, 3, stride=2, padding=1)
+        else:
+            self.conv1 = Conv2d_BN(in_c, out_c, 3, stride=1, padding=1)
+
+        self.conv2 = nn.Conv2d(out_c, out_c, 3, stride=1, padding=1)
+        self.bn = nn.BatchNorm2d(out_c)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        '''
+        conv_bn 3*3 -> conv_bn 3*3
+
+        :param x: input
+        '''
+        residual = x
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.bn(x)
+
+        if self.downsample:
+            residual = self.downsample(residual)
+
+        x = x + residual
+        x = self.relu(x)
+
+        return x
+
+
 
 def Conv2d_BN(in_channels, out_channels, kernel_size=3, stride=1, padding=0, bias=True):
     '''
@@ -29,13 +72,10 @@ class ResNet(nn.Module):
             nn.MaxPool2d(3, stride=2, padding=1)
         )
 
-        in_c = 64
-        self.layer1 = self._make_layer(in_c, block, layers[0], first_layer=True)
-        self.layer2 = self._make_layer(in_c, block, layers[1])
-        in_c *= 2
-        self.layer3 = self._make_layer(in_c, block, layers[2])
-        in_c *= 2
-        self.layer4 = self._make_layer(in_c, block, layers[3])
+        self.layer1 = self._make_layer(64, block, layers[0], first_layer=True)
+        self.layer2 = self._make_layer(128, block, layers[1])
+        self.layer3 = self._make_layer(256, block, layers[2])
+        self.layer4 = self._make_layer(512, block, layers[3])
 
         self.avg_pool = nn.AvgPool2d(7, stride=1)
         self.fc = nn.Linear(512, num_classes)
@@ -55,77 +95,33 @@ class ResNet(nn.Module):
         return x
 
 
-    def _make_layer(self, in_c, block, layer_num, first_layer=False):
+    def _make_layer(self, fisrt_c, block, layer_num, first_layer=False):
         '''
         generate layer use block
 
-        :param in_c number of input channel
+        :param fisrt_c number of first convolution output channel in the paper
         :param block:
         :param layer_num: number of block in layer
         :return: stack nn
         '''
         layer_nn = []
         # stack block
-        # first layer, first stage don't need downsample
-        downsample = True
-        if first_layer:
-            downsample = False
-        layer_nn.append(block(in_c, downsample=downsample))
+        # if input channel don's equals output channel, need downsample
+        downsample = None
+        in_c = fisrt_c
+        if not first_layer:
+            in_c //= 2
+            downsample =  nn.Sequential(
+                nn.Conv2d(in_c, fisrt_c * block.expansions, 1, stride=2, bias=False),
+                nn.BatchNorm2d(fisrt_c * block.expansions)
+            )
+        layer_nn.append(block(in_c, fisrt_c * block.expansions, downsample=downsample))
 
-        if downsample:
-            in_c *= 2
+        # other block's input channel equals output channel
         for _ in range(1, layer_num):
-            layer_nn.append(block(in_c, downsample=False))
+            layer_nn.append(block(fisrt_c * block.expansions, fisrt_c * block.expansions, downsample=False))
 
         return nn.Sequential(*layer_nn)
-
-class BasicBlock(nn.Module):
-
-    def __init__(self, in_c, downsample=False):
-        '''
-        init BasicBlock
-
-        :param in_c: number of input channel
-        :param downsample: True if need downsample in first stage
-        '''
-        super(BasicBlock, self).__init__()
-        self.downsample = downsample
-
-        if downsample:
-            # channel * 2, size / 2
-            out_c = in_c * 2
-            self.downsample = nn.Sequential(
-                nn.Conv2d(in_c, out_c, 3, stride=2, padding=1, bias=False),
-                nn.BatchNorm2d(out_c)
-            )
-            self.conv1 = Conv2d_BN(in_c, out_c, 3, stride=2, padding=1)
-        else:
-            out_c = in_c
-            self.conv1 = Conv2d_BN(in_c, out_c, 3, stride=1, padding=1)
-
-        self.conv2 = nn.Conv2d(out_c, out_c, 3, stride=1, padding=1)
-        self.bn = nn.BatchNorm2d(out_c)
-        self.relu = nn.ReLU()
-
-
-    def forward(self, x):
-        '''
-        conv_bn 3*3 -> conv_bn 3*3
-
-        :param x: input
-        '''
-        residual = x
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.bn(x)
-
-        if self.downsample:
-            residual = self.downsample(residual)
-
-        x = x + residual
-        x = self.relu(x)
-
-        return x
 
 
 def ResNet34():
