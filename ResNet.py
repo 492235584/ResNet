67eb92ec8698a422bc7c1ee89301f957cadedbdc
492235584 <492235584@qq.com>
2019-07-03 13:54:35 +0800

    BottleBlock and resnet50

--- ResNet.py
+++ ResNet.py
@@ -3,22 +3,19 @@ import torch.nn as nn
 class BasicBlock(nn.Module):
     expansions = 1  # how many time increase between first convolution and last convolution
 
-    def __init__(self, in_c, out_c, downsample):
+    def __init__(self, in_c, out_c, downsample, stride=1):
         '''
         init BasicBlock
 
         :param in_c: number of input channel
         :param out_c: number of output channel
         :param downsample: convolution of downsample
+        :param stride: stride in the first conv
         '''
         super(BasicBlock, self).__init__()
         self.downsample = downsample
 
-        if downsample:
-            self.conv1 = Conv2d_BN(in_c, out_c, 3, stride=2, padding=1)
-        else:
-            self.conv1 = Conv2d_BN(in_c, out_c, 3, stride=1, padding=1)
-
+        self.conv1 = Conv2d_BN(in_c, out_c, 3, stride=stride, padding=1)
         self.conv2 = nn.Conv2d(out_c, out_c, 3, stride=1, padding=1)
         self.bn = nn.BatchNorm2d(out_c)
         self.relu = nn.ReLU()
@@ -43,6 +40,47 @@ class BasicBlock(nn.Module):
         return x
 
 
+class BottleBlock(nn.Module):
+    expansions = 4  # how many time increase between first convolution and last convolution
+
+    def __init__(self, in_c, out_c, downsample, stride=1):
+        '''
+        init BottleBlock
+
+        :param in_c: number of input channel
+        :param out_c: number of output channel
+        :param downsample: convolution of downsample
+        :param stride: stride in the first conv
+        '''
+        super(BottleBlock, self).__init__()
+        self.downsample = downsample
+
+        bollte_c = out_c // 4
+        self.conv1 = Conv2d_BN(in_c, bollte_c, 1, stride=stride)
+        self.conv2 = Conv2d_BN(bollte_c, bollte_c, 3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(bollte_c, out_c, 1, stride=1)
+        self.bn = nn.BatchNorm2d(out_c)
+        self.relu = nn.ReLU()
+
+
+    def forward(self, x):
+        '''
+        conv_bn 3*3 -> conv_bn 3*3
+
+        :param x: input
+        '''
+        residual = x
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
+        if self.downsample:
+            residual = self.downsample(residual)
+
+        x = x + residual
+        x = self.relu(x)
+
+        return x
 
 def Conv2d_BN(in_channels, out_channels, kernel_size=3, stride=1, padding=0, bias=True):
     '''
@@ -78,7 +116,7 @@ class ResNet(nn.Module):
         self.layer4 = self._make_layer(512, block, layers[3])
 
         self.avg_pool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear(512, num_classes)
+        self.fc = nn.Linear(512 * block.expansions, num_classes)
 
     def forward(self, x):
         x = self.stem(x)
@@ -108,22 +146,45 @@ class ResNet(nn.Module):
         # stack block
         # if input channel don's equals output channel, need downsample
         downsample = None
-        in_c = fisrt_c
-        if not first_layer:
-            in_c //= 2
-            downsample =  nn.Sequential(
+
+        # first block
+        if block == BasicBlock:
+            if first_layer:
+                in_c = fisrt_c
+                layer_nn.append(block(in_c, fisrt_c * block.expansions, downsample=downsample, stride=1))
+            else:
+                in_c = fisrt_c // 2
+                downsample = nn.Sequential(
+                    nn.Conv2d(in_c, fisrt_c * block.expansions, 1, stride=2, bias=False),
+                    nn.BatchNorm2d(fisrt_c * block.expansions)
+                )
+                layer_nn.append(block(in_c, fisrt_c * block.expansions, downsample=downsample, stride=2))
+        elif block == BottleBlock:
+            # BottleBlock must down sample in first block
+            if first_layer:
+                in_c = fisrt_c
+                downsample =  nn.Sequential(
+                    nn.Conv2d(in_c, fisrt_c * block.expansions, 1, stride=1, bias=False),
+                    nn.BatchNorm2d(fisrt_c * block.expansions)
+                )
+                layer_nn.append(block(in_c, fisrt_c * block.expansions, downsample=downsample, stride=1))
+            else:
+                in_c = fisrt_c * 2
+                downsample = nn.Sequential(
                 nn.Conv2d(in_c, fisrt_c * block.expansions, 1, stride=2, bias=False),
                 nn.BatchNorm2d(fisrt_c * block.expansions)
-            )
-        layer_nn.append(block(in_c, fisrt_c * block.expansions, downsample=downsample))
+                )
+                layer_nn.append(block(in_c, fisrt_c * block.expansions, downsample=downsample, stride=2))
 
         # other block's input channel equals output channel
         for _ in range(1, layer_num):
-            layer_nn.append(block(fisrt_c * block.expansions, fisrt_c * block.expansions, downsample=False))
+                layer_nn.append(block(fisrt_c * block.expansions, fisrt_c * block.expansions, downsample=False))
 
         return nn.Sequential(*layer_nn)
 
 
 def ResNet34():
     return ResNet(BasicBlock, [3, 4, 6, 3])
+def ResNet50():
+    return ResNet(BottleBlock, [3, 4, 6, 3])
 
